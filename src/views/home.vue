67781836304f35de3b428a13a86000faf5a8ec38
JTPeng <404502753@qq.com>
2022-04-03 22:09:02 +0800

    prettierrc配置

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,19 +1,19 @@
 <template>
-	<h1>{{ num }}</h1>
-	<button @click="addNum">addNum</button>
-	<router-link to="/about">about</router-link>
+  <h1>{{ num }}</h1>
+  <button @click="addNum">addNum</button>
+  <router-link to="/about">about</router-link>
 </template>
 
 <script lang="ts" setup>
-	import { ref } from "vue"
-	const num = ref(0)
-	const addNum = () => {
-		num.value += 1
-	}
+  import { ref } from 'vue'
+  const num = ref(0)
+  const addNum = () => {
+    num.value += 1
+  }
 </script>
 
 <style scoped>
-	h1 {
-		color: red;
-	}
+  h1 {
+    color: red;
+  }
 </style>
