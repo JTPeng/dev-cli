51721982c3e842eb762ece6afa08201e719ca70a
JTPeng <404502753@qq.com>
2022-04-06 23:27:51 +0800

    打包构建抽离css为单独文件

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import "babel-polyfill"
-import { createApp } from "vue"
-import { createPinia } from "pinia"
-import router from "./router"
-import App from "./App.vue"
+import 'babel-polyfill'
+import './assets/css/reset.css'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import router from './router'
+import App from './App.vue'
 
 const app = createApp(App)
 // 实例化 Pinia
 const pinia = createPinia()
 
-app.use(router).use(pinia).mount("#app")
+app.use(router).use(pinia).mount('#app')
